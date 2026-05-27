7abe34dd11ddbf45ef7a3018c933251ca60a1537
codebang45 <indoliajatin4@gmail.com>
2026-05-27 19:08:59 +0530

    a set up of project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
 import express from"express";
+import path from "path";
+
 import {ENV} from "./lib/env.js"
 
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+
+const __dirname = path.resolve()
 
 
 app.get("/", (req,res) => {
     res.status(200).json({msg:"sucess from api"})
 })
+
+// make our app ready for deployment
 
 app.listen(ENV.PORT, () => console.log("server is running on 3000"));
